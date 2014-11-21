0dbe5ce674bad485f09f13849525a481166b8796
Ryan Westphal <ryan@trippingthebits.com>
2014-11-21 16:30:11 -0500

    #10077 - scoped directive

--- app/js/directives.js
+++ app/js/directives.js
@@ -7,6 +7,7 @@ var visicatchDirectives = angular.module( 'visicatchDirectives', [] );
 visicatchDirectives.directive( 'ltiTotals', function() {
   return {
     restrict: 'E',
+    scope: '=',
     template: '<p><b>Total Annotations:</b> {{totals.annotations}}<p>'
   };
 } );
