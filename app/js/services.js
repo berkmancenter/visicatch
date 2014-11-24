54dc0735c2c895e20ae601301bceb379eea974ec
Ryan Westphal <ryan@trippingthebits.com>
2014-11-24 15:41:12 -0500

    #10689 - even better test data, remains the same on period change

--- app/js/services.js
+++ app/js/services.js
@@ -5,6 +5,31 @@
 var visicatchSvcs = angular.module( 'visicatchSvcs', [ ] );
 
 visicatchSvcs.factory( 'Activity', function( ) {
+  var data = [];
+  var date = $.now() - ( 86400000 * 365 );
+  var annotationsCreated = 47;
+  var tagsCreated = 2;
+  var activeUsers = 2;
+
+  for ( var day = 0; day < 365; day++ ) {
+    data.push( {
+      date: new Date( date ).toJSON().substr( 0, 10 ),
+      annotationsCreated: annotationsCreated,
+      tagsCreated: tagsCreated,
+      tagsUsed: 84, /* possible? */
+      activeUsers: activeUsers
+    } );
+
+    date += 86400000;
+    annotationsCreated += Math.floor( Math.random() * 10 * ( Math.random() >= 0.5 ? 1 : -1 ) );
+    tagsCreated += Math.floor( Math.random() * 5 * ( Math.random() >= 0.5 ? 1 : -1 ) );
+    activeUsers += Math.floor( Math.random() * 5 * ( Math.random() >= 0.5 ? 1 : -1 ) );
+
+    annotationsCreated = annotationsCreated > 0 ? annotationsCreated : 0;
+    tagsCreated = tagsCreated > 0 ? tagsCreated : 0;
+    activeUsers = activeUsers > 0 ? activeUsers : 0;
+  }
+
   return {
     totals: function( ) {
       return {
@@ -15,33 +40,8 @@ visicatchSvcs.factory( 'Activity', function( ) {
     activity: function( period ) {
       // period - in days
 
-      var data = [];
-      var date = $.now() - ( 86400000 * period );
-      var annotationsCreated = 47;
-      var tagsCreated = 2;
-      var activeUsers = 2;
-
-      for ( var day = 0; day < period; day++ ) {
-        data.push( {
-          date: new Date( date ).toJSON().substr( 0, 10 ),
-          annotationsCreated: annotationsCreated,
-          tagsCreated: tagsCreated,
-          tagsUsed: 84, /* possible? */
-          activeUsers: activeUsers
-        } );
-
-        date += 86400000;
-        annotationsCreated += Math.floor( Math.random() * 10 * ( Math.random() >= 0.5 ? 1 : -1 ) );
-        tagsCreated += Math.floor( Math.random() * 5 * ( Math.random() >= 0.5 ? 1 : -1 ) );
-        activeUsers += Math.floor( Math.random() * 5 * ( Math.random() >= 0.5 ? 1 : -1 ) );
-
-        annotationsCreated = annotationsCreated > 0 ? annotationsCreated : 0;
-        tagsCreated = tagsCreated > 0 ? tagsCreated : 0;
-        activeUsers = activeUsers > 0 ? activeUsers : 0;
-      }
-
       return {
-        activity: data
+        activity: data.slice( -period )
       }
     }
   };
