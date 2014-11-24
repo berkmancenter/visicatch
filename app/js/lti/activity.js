ca7f6611c1d88a31615094111bdf1b60715c3da9
Ryan Westphal <ryan@trippingthebits.com>
2014-11-24 14:42:21 -0500

    #10689 - multi line graph for activity

--- app/js/lti/activity.js
+++ app/js/lti/activity.js
@@ -1,17 +1,40 @@
 ;( function( $, undefined ) {
+
+  var fff = d3.time.format('%Y-%m-%d');
+
+  function convertDate(d) {
+    var fff = d3.time.format('%Y-%m-%d');
+    d[x_accessor] = fff.parse(d[x_accessor]);
+    return d;
+  }
+
   $.fn.ltiActivity = function( activityService ) {
     var activity = activityService.activity();
 
     this.each( function( ) {
-      var converted =  convert_dates( activity.activity, 'date' );
+      var split = [
+        [],
+        [],
+        []
+      ];
+
+      var d;
+      $.each( activity.activity, function( ) {
+        d = fff.parse( this.date );
+
+        split[0].push( { date: d, value: this.annotationsCreated } );
+        split[1].push( { date: d, value: this.tagsCreated } );
+        split[2].push( { date: d, value: this.activeUsers } );
+      } );
+
       data_graphic( {
         title: 'Activity',
-        data: converted,
+        data: split,
         width: 480,
         height: 320,
         target: '#' + this.id,
         x_accessor: 'date',
-        y_accessor: 'annotationsCreated'
+        y_accessor: 'value'
       } );
     } );
   };
