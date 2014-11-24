3e51c47293b62a689f5a182f36d49fe941c247df
Ryan Westphal <ryan@trippingthebits.com>
2014-11-24 12:31:46 -0500

    #10689 - period activity test data

--- app/js/services.js
+++ app/js/services.js
@@ -10,6 +10,28 @@ visicatchSvcs.factory( 'Activity', function( ) {
       return {
         annotations: 20647
       };
+    },
+
+    activity: function( period ) {
+      return {
+        activity: [
+          {
+            date: $.now(),
+            annotationsCreated: 47,
+            tagsCreated: 2,
+            tagsUsed: 84, /* possible? */
+            activeUsers: 2
+          },
+
+          {
+            date: $.now() - 86400,
+            annotationsCreated: 20,
+            tagsCreated: 3,
+            tagsUsed: 23,
+            activeUsers: 3
+          }
+        ]
+      }
     }
   };
 } );
